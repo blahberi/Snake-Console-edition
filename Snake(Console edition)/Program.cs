94339e43cfbc032226dd5e67fb417ab00c8cfa81
blahberi <along.blari@gmail.com>
2021-02-18 19:20:40 +0200

    Add project files.

---
+++ Snake(Console edition)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Snake_Console_edition_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
