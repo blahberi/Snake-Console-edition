3599749047c1fc552712587b3c357464b52d116f
blahberi <along.blari@gmail.com>
2021-02-21 11:06:18 +0200

    snake for console is rather complicated. so im going to create a snake in forms first and then in console.

--- Snake(Console edition)/Program.cs
+++ Snake(Console edition)/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
 
 namespace Snake_Console_edition_
 {
     class Program
     {
-        static void Main(string[] args)
+
+        private static void DrawScreen()
         {
-            Console.WriteLine("Hello World!");
+            //Console.Clear();
+            Console.SetCursorPosition(_left, _top);
+            Console.Write('*');
         }
     }
 }
---
+++ snake/Snake.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Snake
+    {
+        public int direction;
+        /*
+          0 - up
+          1 - down
+          2 - left
+          3 - right
+        */
+        public int[] head;
+        public int[] tail;
+        public Corner[] corners;
+        public Snake()
+        {
+            this.direction = direction;
+            this.head = head;
+            this.tail = tail;
+            this.corners = corners;
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
